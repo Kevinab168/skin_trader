786f5109c0a3fcf46772e4984ee8eecdc3e72778
Kevin <kevin.abraham1688@gmail.com>
2020-12-10 19:37:44 -0500

    Add full tests for CRUD Features of Items and Users

--- skin_trader/items/tests/functional/test_api_items.py
+++ skin_trader/items/tests/functional/test_api_items.py
@@ -1,13 +1,60 @@
 from skin_trader.items.models import Item
 
 
-def test_create_item(db, api_client, item_factory):
+def test_get_item(db, api_client, item_factory):
     item = item_factory.create()
-    retrieved_item = Item.objects.first()
-    response = api_client.get(f'http://localhost8000/api/items/{item.id}/')
+    response = api_client.get(f'http://localhost:8000/api/items/{item.id}/')
     assert response.status_code == 200
-    data = response.json()
-    assert data.get('name') == retrieved_item.name
-    assert data.get('label') == retrieved_item.label
-    assert data.get('description') == retrieved_item.description
-    assert data.get('price') == retrieved_item.price
+    assert Item.objects.filter(name=item.name).exists()
+
+
+def test_create_item(db, api_client, item_factory):
+    name = 'TEST-NAME'
+    label = 'TEST-LABEL'
+    description = 'TEST-DESCR'
+    game = 'TEST-GAME'
+    id_number = '123'
+    price = '1'
+    response = api_client.post(
+        'http://localhost:8000/api/items/',
+        {
+            'name': name,
+            'label': label,
+            'description': description,
+            'game': game,
+            'id_number': id_number,
+            'price': price,
+        },
+        format='json',
+    )
+    assert response.status_code == 201
+    retrieved_item = Item.objects.filter(name=name)[0]
+    assert retrieved_item.label == label
+    assert retrieved_item.description == description
+
+
+def test_update_item(db, api_client, item_factory):
+    item = item_factory.create()
+    updated_name = 'TEST'
+    updated_label = 'TEST'
+    response = api_client.put(
+        f'http://localhost:8000/api/items/{item.id}/',
+        {
+            'name': updated_name,
+            'label': updated_label,
+            'description': item.description,
+            'price': item.price,
+            'game': item.game,
+            'id_number': item.id_number,
+        },
+        format='json',
+    )
+    assert response.status_code == 200
+    assert Item.objects.filter(name=updated_name).exists()
+
+
+def test_delete_item(db, api_client, item_factory):
+    item = item_factory.create()
+    response = api_client.delete(f'http://localhost:8000/api/items/{item.id}/')
+    assert response.status_code == 204
+    assert not Item.objects.first()
--- skin_trader/users/tests/functional/test_api_user.py
+++ skin_trader/users/tests/functional/test_api_user.py
@@ -1,6 +1,13 @@
 from skin_trader.users.models import User
 
 
+def test_user_api(db, api_client, user):
+    response = api_client.get(f'http://localhost:8000/api/users/{user.id}/')
+    assert response.status_code == 200
+    data = response.json()
+    assert data.get('username') == user.username
+
+
 def test_create_user(db, api_client):
     username = 'test'
     password = 'test'
@@ -13,11 +20,26 @@ def test_create_user(db, api_client):
     assert User.objects.filter(username=username).exists()
 
 
-def test_user_api(db, api_client, user):
-    response = api_client.get(f'http://localhost:8000/api/users/{user.id}/')
+def test_update_user(db, api_client, user_factory):
+    user = user_factory.create()
+    updated_username = 'Test'
+    response = api_client.put(
+        f'http://localhost:8000/api/users/{user.id}/',
+        {
+            'username': updated_username,
+            'password': user.password,
+        },
+        format='json',
+    )
     assert response.status_code == 200
-    data = response.json()
-    assert data.get('username') == user.username
+    assert User.objects.filter(username=updated_username).exists()
+
+
+def test_delete_user(db, api_client, user_factory):
+    user = user_factory.create()
+    response = api_client.delete(f'http://localhost:8000/api/users/{user.id}/')
+    assert response.status_code == 204
+    assert not User.objects.first()
 
 
 def test_many_users(db, api_client, user_factory):
